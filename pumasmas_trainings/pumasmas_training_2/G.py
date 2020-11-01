fa0dab538f40c3408e676b2f8a4ce33c4e95a697
Joshua Pedrero <yoshibrightside@ciencias.unam.mx>
2020-10-31 21:43:52 -0600

    pu++ training session 2

--- pumasmas_trainings/pumasmas_training_2/G.py
+++ pumasmas_trainings/pumasmas_training_2/G.py
@@ -1,5 +1,6 @@
+# Unfinished :(
+
 import math
-import sys
 
 def get_min_path(node):
     if node == start:
@@ -12,21 +13,23 @@ def get_min_path(node):
     min_paths[node] = ans
     return ans
 
-sys.setrecursionlimit(20)
 aux = list(map(int, input().split()))
 while (aux != [0, 0 , 0, 0]):
-    sus = [[1, 2, 1], [2, 3, 1], [3, 4, 1], [4, 1, 1]]#[[2,4,78],[1,0,1],[0,2,0],[3,2,59],[4,2,85],[4,1,22],[1,3,23],[0,3,43],[3,4,75],[4,0,15],[0,4,91],[3,0,16],[2,1,98],[2,3,22],[4,3,31],[0,1,0],[1,4,4],[3,1,51],[2,0,36],[1,2,59]]
-    edges, start = [(0, math.inf) for _ in range(aux[0])], aux[3]
-    for i in range(len(sus)):
-        edge = sus[i]
+    edges, start, sus = [(0, math.inf) for _ in range(aux[0])], aux[3], []
+    for _ in range(aux[1]):
+        sus.append(list(map(int, input().split())))
+    sus.sort(key=lambda x: x[2], reverse=True)
+    while sus:
+        edge = sus.pop()
         # According to Edmonds', discard edges to root
-        print('trying', edge)
-        if edge[1] != start and edges[edge[0]] != (edge[1], edge[2]) and edge[0] != edge[1]:
+        if edge[1] != start and (edges[edge[0]][1] == math.inf or edges[edge[0]][1] == math.inf):
             edges[edge[1]] = min((edge[0], edge[2]), edges[edge[1]], key=lambda x: x[1])
-            print(edges[edge[1]], 'defined for', edge[1])
     min_paths = [math.inf for _ in range(aux[0])]
     min_paths[start] = 0
+    for e in edges:
+        print(e)
     for _ in range(aux[2]):
+        visited = {}
         query = int(input())
         ans = get_min_path(query)
         if ans == math.inf:
@@ -34,9 +37,3 @@ while (aux != [0, 0 , 0, 0]):
             continue
         print(ans)
     aux = list(map(int, input().split()))
-
-'''
-[[3,5,78],[2,1,1],[1,3,0],[4,3,59],[5,3,85],[5,2,22],[2,4,23],[1,4,43],[4,5,75],[5,1,15],[1,5,91],[4,1,16],[3,2,98],[3,4,22],[5,4,31],[1,2,0],[2,5,4],[4,2,51],[3,1,36],[2,3,59]]
-5
-5
-'''
