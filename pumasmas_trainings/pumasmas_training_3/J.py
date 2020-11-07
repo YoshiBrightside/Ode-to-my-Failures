f9b11a9054e50ee825fbb762aa7fb8331cee50ad
Joshua Pedrero <yoshibrightside@ciencias.unam.mx>
2020-11-06 23:51:49 -0600

    pu++ training session 3

--- pumasmas_trainings/pumasmas_training_3/J.py
+++ pumasmas_trainings/pumasmas_training_3/J.py
@@ -1,33 +1,34 @@
 import math
 
-def get_min_moves(i, j):
+def get_min_moves(i, j, cur_moves):
     print('visiting', i, j)
     if i == n-1 and j == m-1:
-        return 0
+        return cur_moves
     if memo.get((i, j)):
+        memo[(i,j)] = min(memo[(i,j)], cur_moves)
         return memo[(i,j)]
-    seen[(i, j)] = 1
+    memo[(i, j)] = math.inf
     possibilities = [math.inf]
-    if i + jumps[(i, j)] < n and not seen.get((i + jumps[(i, j)], j)):
-        possibilities.append(get_min_moves(i + jumps[(i, j)], j))
-    if i - jumps[(i, j)] >= 0 and not seen.get((i - jumps[(i, j)], j)):
-        possibilities.append(get_min_moves(i - jumps[(i, j)], j))
-    if j + jumps[(i, j)] < m and not seen.get((i, j + jumps[(i, j)])):
-        possibilities.append(get_min_moves(i, j + jumps[(i, j)]))
-    if j - jumps[(i, j)] >= 0 and not seen.get((i, j - jumps[(i, j)])):
-        possibilities.append(get_min_moves(i, j - jumps[(i, j)]))
+    if i + jumps[(i, j)] < n:
+        possibilities.append(get_min_moves(i + jumps[(i, j)], j, cur_moves))
+    if i - jumps[(i, j)] >= 0:
+        possibilities.append(get_min_moves(i - jumps[(i, j)], j, cur_moves))
+    if j + jumps[(i, j)] < m:
+        possibilities.append(get_min_moves(i, j + jumps[(i, j)], cur_moves))
+    if j - jumps[(i, j)] >= 0:
+        possibilities.append(get_min_moves(i, j - jumps[(i, j)], cur_moves))
     memo[(i, j)] = min(possibilities)+1
     print('ans for', i, j, 'is', memo[(i, j)])
     return memo[(i,j)]
 
 
 n, m = list(map(int, input().split()))
-memo, jumps, seen = {}, {}, {}
+memo, jumps = {}, {}
 for i in range(n):
     aux = input()
     for j in range(len(aux)):
         jumps[(i, j)] = int(aux[j])
-ans = get_min_moves(0, 0)
+ans = get_min_moves(0, 0, 0)
 if ans == math.inf:
     ans = -1
 print(ans)
@@ -38,4 +39,10 @@ print(ans)
 212
 212
 210
+
+4 5
+11111
+19299
+19991
+11110
 '''
