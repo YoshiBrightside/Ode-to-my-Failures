ae0427b39a468f7c95b6cdf62d9fcb71f4efcfb7
Joshua Pedrero <yoshibrightside@ciencias.unam.mx>
2020-11-09 23:14:16 -0600

    Some unsuccessful mods to J.py

--- pumasmas_trainings/pumasmas_training_3/J.py
+++ pumasmas_trainings/pumasmas_training_3/J.py
@@ -1,12 +1,13 @@
-import sys
+import
 import collections
 
-f_in, ans, jumps, seen = input, 0, {}, {}
+
+n, m = list(map(int, input().split()))
+ans, jumps, seen = 0, {}, {}
 queue = collections.deque([(0,0), (-1, -1)]) #-1, -1 is flag for other level
-f_int, pl, ap, ge = int, queue.popleft, queue.append, seen.get
-n, m = list(map(f_int, f_in().split()))
+f_int, pl, ap = int, queue.popleft, queue.append
 for i in range(n):
-    aux = f_in()
+    aux = input()
     for j in range(len(aux)):
         jumps[(i, j)] = f_int(aux[j])
 while queue:
@@ -17,14 +18,18 @@ while queue:
             ap((-1, -1))
             continue
         print(-1)
+        b = datetime.datetime.now() # store final time 
+        print((b-a).total_seconds()) # results 
         quit()
     if i == n-1 and j == m-1:
         print(ans)
+        b = datetime.datetime.now() # store final time 
+        print((b-a).total_seconds()) # results 
         quit()
     seen[(i, j)] = 1
     jump = jumps[(i, j)]
     for x, y in [(i - jump, j), (i + jump, j), (i, j - jump), (i, j + jump)]:
-        if 0 <= x and x < n and 0 <= y and y < m and not ge((x, y)):
+        if 0 <= x and x < n and 0 <= y and y < m and not seen.get((x, y)):
             ap((x, y))
 
 '''
